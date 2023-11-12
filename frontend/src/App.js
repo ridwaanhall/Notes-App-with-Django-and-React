7190167840d45f76ebfa21bba8359057c6400f6d
ridwaanhall <erbyl14@gmail.com>
2023-11-12 15:10:51 +0700

    Setting up React Project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header'
+import NotesListPage from './pages/NotesListPage'
+
+function App() {
+    return (
+        < div className = "App" >
+            <Header />
+            <NotesListPage />
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/NotesListPage.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+
+export const NotesListPage = () => {
+
+    let [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        
+    }, [])
+
+    let getNotes = async () => {
+        fetch
+    }
+
+    return (
+        <div>
+            notes
+        </div>
+    )
+}
+
+export default NotesListPage
